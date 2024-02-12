d2b18bc2454470dcf1e6d6774aca49620bc787b5
seungriyou <seungri0826@gmail.com>
2024-02-13 01:16:57 +0900

    refact: remove useless `Optional`

--- src/main/java/com/peerloop/peerloopapp/domain/auth/service/AuthService.java
+++ src/main/java/com/peerloop/peerloopapp/domain/auth/service/AuthService.java
@@ -9,7 +9,6 @@ import com.peerloop.peerloopapp.domain.auth.repository.AuthRepository;
 import com.peerloop.peerloopapp.global.auth.jwt.JwtProvider;
 import com.peerloop.peerloopapp.global.auth.jwt.dto.TokenDto;
 import com.peerloop.peerloopapp.global.exception.CustomException;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -36,8 +35,7 @@ public class AuthService {
 
         // 2. refresh token DB에 저장
         // memberId에 대한 refreshToken(Auth)이 DB에 존재하면 update, 존재하지 않으면 save
-        Optional<Auth> auth = authRepository.findByMemberId(memberId);
-        auth.ifPresentOrElse(
+        authRepository.findByMemberId(memberId).ifPresentOrElse(
                 existingAuth -> existingAuth.updateRefreshToken(token.refreshToken()),
                 () -> {
                     Auth newAuth = Auth.builder()
@@ -52,8 +50,7 @@ public class AuthService {
     }
 
     public void deleteRefreshToken(Long memberId) {
-        Optional<Auth> auth = authRepository.findByMemberId(memberId);
-        auth.ifPresentOrElse(
+        authRepository.findByMemberId(memberId).ifPresentOrElse(
                 existingAuth -> authRepository.delete(existingAuth),
                 () -> {
                     throw new CustomException(NOT_FOUND_REFRESH_TOKEN);
@@ -63,8 +60,7 @@ public class AuthService {
     }
 
     public Auth getAuthByRefreshToken(String refreshToken) {
-        Optional<Auth> auth = authRepository.findByRefreshToken(refreshToken);
-        return auth
+        return authRepository.findByRefreshToken(refreshToken)
                 .orElseThrow(() -> new CustomException(NOT_FOUND_REFRESH_TOKEN));
     }
 
